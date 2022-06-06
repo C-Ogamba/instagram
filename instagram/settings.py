0ded38910518442e9a2a0aba6249efec393c4f56
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-06-06 08:58:36 +0300

    Dev: install whitenoise

--- instagram/settings.py
+++ instagram/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -47,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'insta'
 ]
 
 ROOT_URLCONF = 'instagram.urls'
@@ -75,8 +80,12 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'social',
+        'USER': 'moringa',
+        'PASSWORD': 'poiuy',
+        'HOST': '',
+        
     }
 }
 
