96642607ae421d773e71154217e1971733799ab6
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-06-06 14:26:42 +0300

    Dev: configure our models.py file

--- instaclone/models.py
+++ instaclone/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
-    title = models.Charfield(max_length=255)
+    title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
 
 
     def __str__(self):
-        return self.title + '|' + self.author
+        return self.title + '|' + str(self.author)
