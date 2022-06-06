1284dea6c857d7090712dbe804dae1ebb91ba244
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-06-06 14:39:13 +0300

    Dev: create our templates

--- instaclone/views.py
+++ instaclone/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from instaclone.models import Post
 
 # Create your views here.
-def index(request):
-    return render(request,'index.html', {})
+# def index(request):
+#     return render(request,'index.html', {})
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'index.html'
+    
