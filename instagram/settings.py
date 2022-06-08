31bce5ad6605046c74ef20cd734f5eff3d959cc2
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-06-08 10:41:10 +0300

    Create tests for our code

--- instagram/settings.py
+++ instagram/settings.py
@@ -73,7 +73,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'instaclone',
     'members',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
